422f2010840200921fdc2dc64730810a2bc86f80
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-05-18 20:25:32 +1000

    working...

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -11,7 +11,11 @@ module MCollective
 
       action "war" do
         validate :source, String
-
+        `wget #{request[:source]} -O /tmp/app.war`
+        `/etc/init.d/tomcat6 stop`
+        `rm -r /var/lib/tomcat6/webapps/*`
+        `mv /tmp/app.war /var/lib/tomcat6/webapps/ROOT.war`
+        `/etc/init.d/tomcat6 start`
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -1,7 +1,29 @@
 import "base.pp"
 
 class app inherits base {
-
+  package { "tomcat6":
+    ensure  => "installed"
+  }
+  
+  service { 'tomcat6':
+      ensure => running,
+      require => [Package['tomcat6'], File['/etc/tomcat6/tomcat6.conf']],
+  }  
+  
+  file { "/etc/tomcat6/tomcat6.conf":
+    ensure => "present",
+    source  => "file://${files_dir}/tomcat6.conf",    
+    owner => tomcat,
+    group => tomcat,
+    require => Package['tomcat6'],
+  }  
+  
+  file { "/var/db/prevayler":
+    ensure => "directory",
+    owner => tomcat,
+    group => tomcat,
+    require => File["/etc/tomcat6/tomcat6.conf"],
+  }
 }
 
 include app
